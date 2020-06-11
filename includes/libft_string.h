fc28f9a1bc28a0dfa2995a7e1daf9ac0924f3a4d
Mylhow <dgascon@student.42lyon.fr>
2020-06-11 09:34:18 +0200

    Add fcn memrchr

--- includes/libft_string.h
+++ includes/libft_string.h
@@ -7,4 +7,5 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memrchr(const void *s, int c, size_t n);
 #endif
---
+++ srcs/string/ft_memrchr.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void	*ft_memrchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	while (n--)
+	{
+		if (str[n] == c)
+			return (&str[n]);
+	}
+	return (0);
+}

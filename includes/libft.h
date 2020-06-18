04d0a87e44d4eac68f107bb2527d989f28b1976a
mylhow <dgascon@student.42lyon.fr>
2020-06-18 21:04:51 +0200

    Add True, False, Base..

--- includes/libft.h
+++ includes/libft.h
@@ -3,4 +3,13 @@
 
 # define EXIT_SUCCESS 0
 # define EXIT_FAILURE 1
+
+# define TRUE 1
+# define FALSE 0
+
+# define BASE10 "0123456789"
+# define BASE16U "0123456789ABCDEF"
+# define BASE16L "0123456789abcdef"
+# define BASE8 "01234567"
+
 #endif

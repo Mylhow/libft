4dbed992a7f89f25729b4e3536d171533b1d73b5
mylhow <dgascon@student.42lyon.fr>
2020-06-18 19:17:32 +0200

    Add fcn lstsize

--- includes/libft_list.h
+++ includes/libft_list.h
@@ -16,4 +16,5 @@ void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstlast(t_list *lst);
 t_list			*ft_lstnew(void *content);
 t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int				ft_lstsize(t_list *lst);
 #endif

c21cb5e84ed85e51fbc9ed434787c0a05bedc05f
mylhow <dgascon@student.42lyon.fr>
2020-06-18 18:30:35 +0200

    Add fcn Strcmp

--- includes/libft_string.h
+++ includes/libft_string.h
@@ -13,4 +13,5 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memccpy(void *dst, void const *src, int c, size_t n);
 char	*ft_strdup(const char *s1);
+int		ft_strcmp(char *s1, char *s2);
 #endif
---
+++ srcs/string/ft_strcmp.c
@@ -0,0 +1,24 @@
+#include "libft_string.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned char	*st1;
+	unsigned char	*st2;
+	int				i;
+	int				i2;
+
+	i = 0;
+	i2 = 0;
+	st1 = (unsigned char *)s1;
+	st2 = (unsigned char *)s2;
+	while (s1[i] && s2[i2])
+	{
+		if (s1[i] != s2[i2])
+		{
+			return (st1[i] - st2[i2]);
+		}
+		i2++;
+		i++;
+	}
+	return (st1[i] - st2[i2]);
+}

7d26ce70dd04677aff9d744a506052eebae6652d
mylhow <dgascon@student.42lyon.fr>
2020-06-18 18:20:26 +0200

    Add fcn Memcmp

---
+++ srcs/string/ft_memcmp.c
@@ -0,0 +1,19 @@
+#include "libft_string.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*st1;
+	const unsigned char	*st2;
+	size_t				i;
+
+	st1 = s1;
+	st2 = s2;
+	i = 0;
+	while (i < n)
+	{
+		if (*(st1 + i) != *(st2 + i))
+			return (*(st1 + i) - *(st2 + i));
+		i++;
+	}
+	return (0);
+}
